fd6d83c9614463295c66f715b7841f3766ce413c
kekkopel <66108159+kekkpel@users.noreply.github.com>
2021-12-23 17:26:59 +0100

    Update virus.py

--- virus/virus.py
+++ virus/virus.py
@@ -8,6 +8,8 @@ def main_viruses():
     print('Warning I am making this only for fun')
     print('I am not responsable for what you are going to do')
     print('')
+    print('This is a collection of viruses made by me')
+    print('These viruses have to be converted to exe to work on other pcs')
 
     while True:
         input_vir = input("virus-$ ")
